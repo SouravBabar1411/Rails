cf803109faad4828b0873ea86861bf23c2972356
sourav1411 <babarsourav12@gmail.com>
2022-03-25 10:46:08 +0530

    added even select odd select buttons

--- app/assets/javascripts/drop.js
+++ app/assets/javascripts/drop.js
@@ -14,6 +14,22 @@ function deselectAll() {
     }
 }
 
+function evenAll() {
+    let number = document.getElementsByName("number");
+    let cityCheckLen = number.length;
+    for (var x = 1; x < cityCheckLen; x += 2) {
+        number[x].checked = true;
+    }
+}
+
+function oddAll() {
+    let number = document.getElementsByName("number");
+    let cityCheckLen = number.length;
+    for (var x = 0; x < cityCheckLen; x += 2) {
+        number[x].checked = true;
+    }
+}
+
 function update() {
     var select = document.getElementById('language');
     var option = select.options[select.selectedIndex];
