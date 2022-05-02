874111b5c7cfb2416252c251dc8e259a560b59b7
Sourav <babarsouav12@gmail.com>
2022-05-02 12:17:35 +0530

    integreated theme

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,29 @@
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, or any plugin's
+// vendor/assets/javascripts directory can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// compiled file. JavaScript code in this file should be added after the last require_* statement.
+//
+// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
+// about supported directives.
+//
+
+//= require rails-ujs
+//= require activestorage
+//= require turbolinks
+//= require plugins/Chart.min
+//= require plugins/bootstrap-datepicker.min
+//= require plugins/jquery.flot
+//= require plugins/jquery.flot.resize
+//= require plugins/jquery.flot.categories
+//= require plugins/jquery.flot.fillbetween
+//= require plugins/jquery.flot.stack
+//= require plugins/jquery.flot.pie
+//= require plugins/off-canvas
+//= require plugins/hoverable-collapse
+//= require plugins/misc
+//= require plugins/dashboard
+//= require_tree .
